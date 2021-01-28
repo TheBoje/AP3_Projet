a5a5da16d4b9a9c2249d4a93fbfe91f9b62c6563
TheBoje <louis.leenar@gmail.com>
2021-01-28 13:36:50 +0100

    ajout plot bst seek

--- src/avl.ml
+++ src/avl.ml
@@ -402,8 +402,40 @@ let suppr_avl_plot(n : int) : float =
   )
 ;;
 
+
+let seek_avl_compute(n : int) : float array * float array =
+  let indices : float array = arr_make(n + 1, 0.0) in
+  let temps : float array = arr_make(n + 1, 0.0) in
+  (
+    for i = 1 to n 
+    do 
+      let rnd_list = random_list_int(i, valeur_max) in
+      let avl = avl_rnd_create(rnd_list) in
+      temps.(i) <- Sys.time();
+      ignore(bst_seek(nth(rnd_list, Random.int(len(rnd_list))), avl));
+      temps.(i) <- Sys.time() -. temps.(i);
+      indices.(i) <- float_of_int(i);
+      done;
+      (temps, indices);
+      )
+;;
+
+let seek_avl_plot(n : int) : float =
+  let init_time : float = Sys.time() in
+  let (temps, indices) : float array * float array = suppr_avl_compute(n) in
+  let repere : t_rep = {orx = 50; ory = 50; extx = 900; exty = 500} in
+  (
+    open_graph(1000, 600);
+    clear_graph();
+    draw_rep(repere);
+    draw_curve(temps, indices, arr_len(indices) - 1, repere);
+    Sys.time() -. init_time;
+    )
+;;
 (*
 insert_avl_plot(1000);;
+suppr_avl_plot(1000);;
 
 *)
-suppr_avl_plot(1000);;
+
+seek_avl_plot(1000);;
